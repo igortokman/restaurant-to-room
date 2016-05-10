9ccfb4105c231afe757638ae02263b99cb344d54
IgorTokman <igortokman1994@ukr.net>
2016-05-10 21:48:32 +0000

    implemented the persisting objects with Mongoose

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var userService = require('../services/user-service');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -16,20 +18,18 @@ router.get('/create', function(req, res, next) {
 });
 
 router.post('/create', function(req, res, next) {
-  var somethingGoesWrong = false;
-  if(somethingGoesWrong){
-
-    var vm ={
-      title: 'create an account',
-      input: req.body,
-      error: 'something went wrong'
-    };
-
-    delete vm.input.password;
-    return res.render('users/create', vm);
-  }
-
-  res.redirect('/orders');
+userService.addUser(req.body, function(err) {
+    if (err) {
+      var vm = {
+        title: 'Create an account',
+        input: req.body,
+        error: 'Something went wrong'
+      };
+      delete vm.input.password;
+      return res.render('users/create', vm);
+    }
+    res.redirect('/orders');
+  });
 });
 
 module.exports = router;
