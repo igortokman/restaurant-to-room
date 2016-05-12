8cc16dca7b58bdf8c0f927f62762c7cbaa332346
IgorTokman <igortokman1994@ukr.net>
2016-05-12 09:45:26 +0000

    realized the configuring client-side routing with AngularJS

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {};
 config.mongoUri = 'mongodb://localhost:27017/rtr';
 config.cookieMaxAge = 30 * 24 * 3600 * 1000;
 
-config.ordrxKey = 'U2xdrXFCHj5Ar3i1Drd1-Kf9G657PSOKuNzHyODti-U';
+config.ordrxKey = 'J3hHrYriiJQ7akUgsmJZGO591e8Gvl-MSwsFwdtpDnA';
 config.address = {
   addr: '288 Coleridge St.',
   city: 'San Francisco',
