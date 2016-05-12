f4bb04a6118f0b3adba55546cf89509417bb52c7
IgorTokman <igortokman1994@ukr.net>
2016-05-12 09:08:47 +0000

    added a JSON API route

--- config.js
+++ config.js
@@ -3,4 +3,13 @@ var config = {};
 config.mongoUri = 'mongodb://localhost:27017/rtr';
 config.cookieMaxAge = 30 * 24 * 3600 * 1000;
 
+config.ordrxKey = 'U2xdrXFCHj5Ar3i1Drd1-Kf9G657PSOKuNzHyODti-U';
+config.address = {
+  addr: '288 Coleridge St.',
+  city: 'San Francisco',
+  zip: '94110',
+  state: 'CA'
+};
+config.phone = '415-555-1234';
+
 module.exports = config;
--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var restrict = require('../auth/restrict')
+var orderService = require('../services/order-service');
 
 router.get('/', restrict, function(req, res, next) {
   if (!req.isAuthenticated()) {
@@ -13,6 +14,15 @@ router.get('/', restrict, function(req, res, next) {
   };
 
   res.render('orders/index', vm);
+});
+
+router.get('/api/restaurants', restrict, function(req, res, next) {
+  orderService.getRestaurants(function(err, restaurants) {
+    if (err) {
+      return res.status(500).json({error: 'Failed to retrieve restaurants'});
+    }
+    res.json(restaurants);
+  });
 });
 
 module.exports = router;
