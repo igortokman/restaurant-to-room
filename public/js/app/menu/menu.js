dfce819dba4bb4a99df2f1e9f9388cdc61710e2d
IgorTokman <igortokman1994@ukr.net>
2016-05-13 15:18:57 +0000

    created the integrating a modal pop-up service

--- public/js/app/menu/menu.js
+++ public/js/app/menu/menu.js
@@ -5,14 +5,25 @@
     .module('app')
     .controller('MenuController', MenuController);
 
-  MenuController.$inject = ['api', '$routeParams'];
+  MenuController.$inject = ['api', '$routeParams', 'ngDialog', '$scope'];
 
-  function MenuController(api, $routeParams) {
+  function MenuController(api, $routeParams, ngDialog, $scope) {
     var vm = this;
 
     api.getRestaurantDetails($routeParams.restId)
       .then(function(data) {
         vm.restaurant = data;
       });
+
+      vm.viewItem = function(item) {
+        vm.activeItem = item;
+        vm.activeItem.options = [];
+
+        ngDialog.open({
+          templateUrl: 'item.html',
+          className: 'ngdialog-theme-default',
+          scope: $scope
+        });
+      };
   }
 }());
