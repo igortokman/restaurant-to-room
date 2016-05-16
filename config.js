063a9381f8a7e6295c0a77dc4ef8adad6f2c5a09
IgorTokman <igortokman1994@ukr.net>
2016-05-16 20:39:33 +0000

    fixed the Mongo connection string

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.mongoUri = 'mongodb://localhost:27017/rtr';
+config.mongoUri = process.env.MONGOLAB_uri || 'mongodb://localhost:27017/rtr';
 config.cookieMaxAge = 30 * 24 * 3600 * 1000;
 
 config.ordrxKey = 'J3hHrYriiJQ7akUgsmJZGO591e8Gvl-MSwsFwdtpDnA';
