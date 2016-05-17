3ea39523c0d68b5a5b7caf38cd1ef9ee3a4e3134
IgorTokman <igortokman1994@ukr.net>
2016-05-17 18:21:18 +0000

    implemented the using gulp for minification

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ mongoose.connect(config.mongoUri);
 
 var app = express();
 
+//app.set('production', process.env === 'production');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
